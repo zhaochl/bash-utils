5e21e7f4c4b259a1c3ee4ae5e9bf69613707bc3b
zcl <zcl@timeplan.cn>
2017-01-20 16:58:11 +0800

    update

--- gen_git_key.sh
+++ gen_git_key.sh
@@ -2,4 +2,6 @@
 cd ~/.ssh
 ssh-keygen -t rsa -C "zcl@timeplan.cn"
 ssh -T git@github.com
+echo 'update .git/conf https->ssh [origin] to  git@github.com:Username/Your_Repo_Name.git'
+echo 'git push test'
 echo 'add git key ok'
