1854844353b76eec31f8f55362a8ec94eeb5f91b
zcl <zcl@timeplan.cn>
2017-01-23 12:15:47 +0800

    monitor ps

--- utils/monitor_process_restart.sh
+++ utils/monitor_process_restart.sh
@@ -1,32 +1,27 @@
-#!/bin/sh
-
-CMD=pgrep
-PPROC=1862
+#!/bin/bash
+# crontab */1 * * * * /bin/bash  monitor_nginx.sh
 #while :
-today_date=`date -d today +"%Y%m%d %H:%M:%S"`
-#echo $today_date
+today_date=`date -d today +"%Y-%m-%d %H:%M:%S"`
+today_day=`date -d today +"%Y-%m-%d"`
+PROGRAM='nginx'
+
 #do
+LOGFILE="/data/logs/crontab/"$PROGRAM"_"$today_day"."log
 
-sn3=`ps -ef | grep ir_ol_cs | grep -v grep |awk '{print $2}'`
-echo $sn3
+pid=`ps -ef | grep $PROGRAM | grep -v 'grep' |grep -v 'tail'|grep -v 'bash'|awk '{print $2}'`
+ps -ef | grep $PROGRAM | grep -v 'grep' |grep -v 'tail'|grep -v 'bash'
+#
+echo "check $PROGRAM at $today_date,pid:$pid" |tee -a $LOGFILE 2>&1
 
-if [ "${sn3}" = "" ]
-#if [ -n "`$CMD $PPROC`" ]
+if [ "${pid}" = "" ]
 then
-	echo "not running" >> t1 2>&1
-	echo $today_date
 
-    cd /jacky_at_yitai/search/cs; sh start_online.sh
+	echo "error,will restart $PROGRAM" |tee -a $LOGFILE 2>&1
+	service nginx start
+	today_restart=`date -d today +"%Y-%m-%d %H:%M:%S"`
+	echo "restart ok $PROGRAM at $today_restart" |tee -a $LOGFILE 2>&1
+
 else
-    echo "running" >> t1 2>&1
-    echo $today_date >> t1 2>&1
-
-    #cd /mnt/cs ; sh start.sh;
+    echo "good,$PROGRAM is running,pid:$pid."|tee -a $LOGFILE 2>&1
 
 fi
-
-#########    sleep 60
-
-#done
-
-
